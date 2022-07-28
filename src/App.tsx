e2a1862cc172ff40fc2b7b06fbabd6db6716004b
yunjink <chloeeekim1109@gmail.com>
2022-07-28 16:42:34 +0900

    feat : dev merge 전 App 내용 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import ScheduleBox from './components/ScheduleBox';
-import ConfirmDeleteModal from './components/ConfirmDeleteModal';
 
 function App() {
-  return (
-    <>
-      <ScheduleBox />
-      <ConfirmDeleteModal />
-    </>
-  );
+  return <></>;
 }
 
 export default App;
