3acdde6dd68d72aef7165d73660e52e172148ac8
yunjink <chloeeekim1109@gmail.com>
2022-07-30 10:02:50 +0900

    conflict : merge conflict solved

--- src/store/atom.ts
+++ src/store/atom.ts
@@ -1,43 +1,34 @@
-import { atom, selector } from "recoil";
-import { getDay, toDate, addDays } from "date-fns";
+import { scheduleService } from '../api/api';
+import { atom, selector } from 'recoil';
+import { getDay, toDate, addDays } from 'date-fns';
 
 const getWeekRangeState = selector({
-  key: "getWeekRangeState",
+  key: 'getWeekRangeState',
   get: () => {
     const now: Date = new Date();
     const day: number = getDay(now);
     const mondayMilliseconds: number = now.setDate(now.getDate() - (day - 1));
     const monday: Date = toDate(mondayMilliseconds);
-    return [
-      monday,
-      addDays(monday, 1),
-      addDays(monday, 2),
-      addDays(monday, 3),
-      addDays(monday, 4),
-      addDays(monday, 5),
-      addDays(monday, 6),
-    ];
+    return [monday, addDays(monday, 1), addDays(monday, 2), addDays(monday, 3), addDays(monday, 4), addDays(monday, 5), addDays(monday, 6)];
   },
 });
 
 export const weekState = atom({
-  key: "weekState",
+  key: 'weekState',
   default: getWeekRangeState,
 });
 
 export const getUrlString = selector({
-  key: "getUrlString",
+  key: 'getUrlString',
   get: ({ get }) => {
     const weekData = get(weekState);
-    const requestUrlString = `?date_gte=${weekData[0].toLocaleDateString()}&date_lte=${weekData[
-      weekData.length - 1
-    ].toLocaleDateString()}`;
+    const requestUrlString = `?date_gte=${weekData[0].toLocaleDateString()}&date_lte=${weekData[weekData.length - 1].toLocaleDateString()}`;
     return requestUrlString;
   },
 });
 
 const getScheduleState = selector({
-  key: "getScheduleState",
+  key: 'getScheduleState',
   get: async ({ get }) => {
     const url = get(getUrlString);
     const schedule = scheduleService.get(url).then((response) => {
@@ -48,6 +39,6 @@ const getScheduleState = selector({
 });
 
 export const scheduleState = atom({
-  key: "scheduleState",
+  key: 'scheduleState',
   default: getScheduleState,
 });
